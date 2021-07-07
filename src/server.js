867cefda5976e0e81c309b4ed6c9618acc8ca397
cjdiniz <cjsdiniz@outlook.com>
2021-07-07 17:42:22 +0100

    Act

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const route = require('./route')
-const path =  require('path')
+const path = require('path')
+
 const server = express()
 
 server.set('view engine', 'ejs')
 
-// server.set('views', './views')
-server.set('views',  path.join(__dirname,'views'))
+server.use(express.static("public"))
+
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
 
 server.use(route)
 
-server.listen(3000, () => console.log("Em execução"))
-
- 
+server.listen(3000, () => console.log("RODANDO"))
